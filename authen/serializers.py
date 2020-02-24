bdfb0f8bddf38850b245fee4a1bacd25220115ff
Costigun <maximneveraa@gmail.com>
2020-02-24 14:17:18 +0600

    Delete useless serializer

--- authen/serializers.py
+++ authen/serializers.py
@@ -1,20 +1,3 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from .models import CustomUser
-User = get_user_model()
-
-class CustomUserSerializer(serializers.ModelSerializer):
-    validation_code = serializers.CharField(max_length=4,allow_null=True)
-    # password = serializers.CharField(max_length=20,read_only=True)
-
-    def create(self,validated_data,*args,**kwargs):
-        dict = User.objects.create_user(
-            username=validated_data['username'],
-            password=validated_data['validation_code'],
-            validation_code=validated_data['validation_code'],
-    )
-        return dict
-
-    class Meta:
-        model = CustomUser
-        fields = ('username','validation_code')
