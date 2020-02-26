ef2740a577fc3e54c9f78cc91eb2f780838334db
Costigun <maximneveraa@gmail.com>
2020-02-26 20:45:41 +0600

    Add permission to service

--- service/views.py
+++ service/views.py
@@ -1,4 +1,4 @@
-from rest_framework import status
+from rest_framework import status, permissions
 from rest_framework.generics import ListCreateAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -11,6 +11,9 @@ from .models import *
 
 
 class ServiceView(ModelViewSet):
-    permission_classes = IsOwnerOrReadOnly
+    permission_classes = (permissions.IsAuthenticated,)
     queryset = Service.objects.all()
     serializer_class = ServiceSerializer
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
