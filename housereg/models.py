d68b19e4292c389068d0d63fc4e47489ff4da9ed
Alex <domkom.app@gmail.com>
2020-03-06 16:48:37 +0600

    Wrote persons counter

--- housereg/models.py
+++ housereg/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 from users.models import User
 from authen.models import CustomUser
@@ -7,7 +9,7 @@ from authen.models import CustomUser
 class House(models.Model):
     address = models.CharField(max_length=256)
     house_number = models.CharField(max_length=16)
-    people = models.IntegerField()
+    people = models.IntegerField(default=0, blank=True, null=True)
 
     class Meta:
         verbose_name = 'Дом'
@@ -18,8 +20,9 @@ class House(models.Model):
 
 
 class UsersInHouse(models.Model):
-    person = models.ForeignKey(CustomUser, on_delete=models.CASCADE, default=None)
+    person = models.ForeignKey(CustomUser, on_delete=models.CASCADE, default=None, related_name='person')
     house = models.ForeignKey(House, on_delete=models.CASCADE, default=None)
+    people = models.IntegerField(default=None)
 
     class Meta:
         verbose_name = 'Житель'
@@ -27,3 +30,11 @@ class UsersInHouse(models.Model):
 
     def __str__(self):
         return '%s' % self.person
+
+
+@receiver(post_save, sender=UsersInHouse)
+def persons_count(sender, instance, created, **kwargs):
+    users, created = House.objects.get_or_create()
+    users.people += instance.people
+    print(users.people)
+    users.save()
